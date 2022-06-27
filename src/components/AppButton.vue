0bf6a86a0edb21a92cf21b2a370c5bd0622c77b9
atisheko <tema.tishenko1993@yadnex.ru>
2022-06-27 18:48:02 +0300

    first commit

---
+++ src/components/AppButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button class="button">Загрузить данные</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'AppButton'
+}
+</script>
+
+<style scoped>
+
+</style>
