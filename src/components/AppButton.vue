83983f77934d3bd30a4a38724711de8a8cd6e6ba
ATishenko <tema.tishenko1993@yandex.ru>
2022-06-28 11:34:12 +0300

    хорошего дня

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,15 +1,53 @@
 <template>
     <div>
-        <button class="button">Загрузить данные</button>
+
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name:'AppButton'
+    name: 'AppButton',
+    data() {
+        return {
+            responseData:null
+        }
+    },
+    methods: {
+        getData() {
+            axios({
+                method: 'get',
+                url: 'https://jsonplaceholder.typicode.com/todos',
+            }).then((response)=>{
+                let test = JSON.parse(JSON.stringify(response)).data
+                console.log(test)
+                // this.responseData = [...test]
+                this.responseData =  [...Object.assign(response).data]
+                console.log( this.responseData)
+            }).catch((err)=>{
+                console.log('ERROR',err)
+            })
+        }
+    }
 }
 </script>
 
 <style scoped>
+.button {
+    padding: 1rem;
+    border: 0px;
+    border-radius: 10px;
+    background-color: mediumseagreen;
+    font-weight: 500;
+    margin-bottom: 10px;
+    color: white;
 
+}
+
+button:hover {
+    cursor: pointer;
+    font-weight: 600;
+    outline: 1px solid greenyellow;
+}
 </style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="card">
+        <h2 class="card__user">Пользователь {{ item.userId }}</h2>
+        <div class="card__count">
+            <span>{{ item.count }}/</span><span>{{ item.totalCount }}</span>
+        </div>
+        <p>№{{ item.id }}</p>
+        <ul class="card__titles">
+            <li class="card__title" v-for="(itemTitle,idx) in item.titles" :key="idx">
+                <p>{{itemTitle}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'AppCard',
+    props:['item'],
+    computed:{
+       // totalCompleted(){
+       //     let result = this.item
+       //     return result
+       // }
+    }
+}
+</script>
+
+<style scoped>
+    .card{
+        width: 200px;
+        background-color: lightgray;
+        height: 100px;
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 10px;
+        border-radius: 12px;
+        padding: 5px;
+        overflow: hidden;
+        overflow-y: scroll;
+
+    }
+    .card__user{
+        font-size: 12px;
+    }
+    .card__titles{
+        list-style-type: circle;
+
+    }
+    .card__title{
+        font-size: 11px;
+    }
+</style>
